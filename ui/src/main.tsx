3b4e2bb91952d6fe916c4f11c56241c9f63d851b
kh-kim <keonhok0315@gmail.com>
2026-05-09 01:34:00 +0900

    Wrap app in React Router

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter } from "react-router-dom";
 import { App } from "./App";
 import { AuthGate } from "./features/auth/AuthGate";
 import "./tailwind.css";
@@ -17,9 +18,11 @@ const queryClient = new QueryClient({
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <AuthGate>
-        <App />
-      </AuthGate>
+      <BrowserRouter>
+        <AuthGate>
+          <App />
+        </AuthGate>
+      </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>,
 );
